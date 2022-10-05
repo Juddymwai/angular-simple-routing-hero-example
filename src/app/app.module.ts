6ca399419accbfdd03cf38099fc0863e58465b5b
Judy Mwai <juddiemukami@gmail.com>
2022-10-05 15:26:45 +0300

    adds an image in hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 
+const appRoutes: Routes =[
+  {path: 'crisis-center', component: CrisisListComponent},
+  { path: 'heroes', component: HeroListComponent}
+];
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes, 
+      { enableTracing: true} // debugging purposes
+    )
+
+  ],
   declarations: [
     AppComponent,
     CrisisListComponent,
     HeroListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
